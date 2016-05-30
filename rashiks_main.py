953fd762072a63062ae7ba23203ee451b4b760f4
Rashid <fakhrtd@gmail.com>
2016-05-31 01:34:35 +0300

    Update rashiks_main.py

--- rashiks_main.py
+++ rashiks_main.py
@@ -19,25 +19,3 @@ xu.UpdateDicts(new_xmlstr)
         # repr(kee)
         # repr(vaals)
         # exit()
-
-
-fout_path = 'D:/program_backup/python-sources/data/output/'
-fout = open(fout_path+'test'+'.txt','a')
-tu = iv.topstIds.items()
-tu.sort()
-#print tu
-for kee, vaals in tu :
-    fout.write(str(kee) + ': ')
-    for usr in vaals :
-        try:
-            fout.write('[' + usr.encode('utf-8') + ']')
-        except:
-            print '=== shit in writing user to file ===', kee, usr
-            print 'user_name type:', type(usr), 'user_name:', str(usr)
-            fout.close()
-            raise
-           # print usr            
-            exit()
-    fout.write('\n')
-fout.close()
-
