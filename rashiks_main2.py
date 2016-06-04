2fbeb956ac533b2bebd8e39442b7a70b4775c239
Rashid <fakhrtd@gmail.com>
2016-06-04 22:41:15 +0300

    Update rashiks_main2.py

--- rashiks_main2.py
+++ rashiks_main2.py
@@ -1,30 +1,44 @@
 # -*- coding: utf-8 -*-
+'''
+Realization of board-xml-database loading, real-time updating;
+topicstarter's/commentator's messages's attributies processing;
+obtaining list of all users who posted in the topicstarter's message with specific id
+'''
 import time
 import xml_update_2_0 as xu
 
-#old_lastId = -1
-
-#patch to print unicode into console
+#patch requicked to print unicode strings to console
 #import win_unicode_console
 #win_unicode_console.enable()
 
 #AwesomeInitOfDatabase()
-# ^^^ We truly needs this function
-xu.BadInit()
-old_lastId = xu.GetLastMessageId()
-while True:
-    time.sleep(10)
+# ^^^ We truly needs this function (need further discussion)
 
+# download and processing last 1000 messages from online-board-xml-database, updating Model, MyDicts using this data
+old_lastId = xu.BadInit()
+
+# real-time Model and myDicts update
+while True:
+    # waiting some time between checking the forum for new messages
+    time.sleep(10)
+    
+    # check forum for new message and process it if it's available
     if xu.CheckForNewMessage(old_lastId):
-        lastId = xu.GetLastMessageId()        
+        lastId = xu.GetLastMessageId()
+        
+        # download new xmls appeared in time of sleep
         new_xmlstr = xu.DownloadNewXMLs(old_lastId,lastId)
         old_lastId = lastId
         
-        # gets message data and updates myDicts
+        # gets message data and updates topstIds, commIds, topicIdStack
         xu.XMLstrProcessing(new_xmlstr)
         print "Message accepted: ", old_lastId
-    else: print "Waiting messages, last processid message id:", old_lastId
+    
+    else: print "....... waiting messages ....... last mesid:", old_lastId
+    
     # update list of usernames
     new_list = xu.UpdateListOfUserNames(1)
     print "Updating Model with:", new_list, '\n'
+    
+    M.modelUpdate(new_list)
     xu.DebugSaveToFile(new_list)
