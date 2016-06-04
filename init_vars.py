0b565a9c02f90ef7cc4cecc7ec4a6c9a8307b49a
Rashid <fakhrtd@gmail.com>
2016-06-04 21:38:49 +0300

    Update init_vars.py

--- init_vars.py
+++ init_vars.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
-topstIds = {}
+'''
+The module initialize some global data structures used in xml_update_2_0 module
+'''
+
+topstIds = {} # 
 commIds = {}
 topicIdStack = []
