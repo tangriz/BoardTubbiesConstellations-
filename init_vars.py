c9f7312b091dd6ddbc175f10ba48d308401f108d
Rashid <fakhrtd@gmail.com>
2016-06-06 22:53:20 +0300

    Update init_vars.py

--- init_vars.py
+++ init_vars.py
@@ -2,7 +2,17 @@
 '''
 The module initialize some global data structures used in xml_update_2_0 module
 '''
+# init Topic Starter message indices dictionary
+# key - Topic Starter message index
+# values - list of all commentators message indices
+topstIds = {}
 
-topstIds = {} # 
+# init Topic Starter message indices dictionary
+# key - commentator message index
+# value - Topic Starter message index
 commIds = {}
+
+# init Stack of Topic Starter message indices
+# key - Topic Starter message index
+# value - date/time of the Topic Starter post
 topicIdStack = []
